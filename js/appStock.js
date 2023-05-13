93213f8f4e825b6901fc5381a8cf4107360e010b
SergioV <85177990+Svisentin@users.noreply.github.com>
2023-05-13 00:14:01 -0300

    Modifico nombres de img y trabajo con archivo JSON

--- js/appStock.js
+++ js/appStock.js
@@ -8,43 +8,13 @@ divContainer_stock.style.alignItems = "center"
 main.appendChild(divContainer_stock);
 
 // Obtener los productos del almacenamiento local #lista-productos
-// const productos = JSON.parse(localStorage.getItem("listaProductos")) || [];
+
 
 // Ordenar los productos por el código
-// productos.sort((a, b) => a.codigo.localeCompare(b.codigo));
+// ;
 
 // Recorrer el array de productos y agregarlos a la lista
-/* productos.forEach((producto) => {
-  const div_card = document.createElement("div");
-  div_card.classList.add("card_stock");
-  
-  const codigoProducto = document.createElement("p");
-  codigoProducto.innerHTML = `Código: ${producto.codigo}`;
-  div_card.appendChild(codigoProducto);
 
-  const imgProducto = document.createElement("img")
-  imgProducto.src = `../img/${producto.codigo}.png`;
-  imgProducto.width = 150
-  imgProducto.height = 150
-  imgProducto.style.borderRadius = "5px"
-  imgProducto.style.display = "block"
-  imgProducto.style.margin = "auto" 
-  imgProducto.style.marginBottom = "10px";
-  imgProducto.style.marginTop = "10px";
-
-  imgProducto.style.objectFit = "cover";
-  div_card.appendChild(imgProducto)
-  
-  const stockProducto = document.createElement("p");
-  stockProducto.innerHTML = `Stock: ${producto.stock}`;
-  div_card.appendChild(stockProducto);
-  
-  const precioProducto = document.createElement("p");
-  precioProducto.innerHTML = `Precio Unitario: $${producto.precioUnitario}`;
-  div_card.appendChild(precioProducto);
-  
-  divContainer_stock.appendChild(div_card);
-}); */
 
 
 let productos = [];
@@ -56,6 +26,41 @@ async function obtenerProductos() {
 }
 
 obtenerProductos().then(() => {
+  productos.forEach((producto) => {
+    const div_card = document.createElement("div");
+    div_card.classList.add("card_stock");
+
+    const idProducto = document.createElement("p");
+    idProducto.innerHTML = `Código: ${producto.id}`;
+    div_card.appendChild(idProducto);
+    
+    const nombreProducto = document.createElement("p");
+    nombreProducto.innerHTML = `${producto.nombre}`;
+    div_card.appendChild(nombreProducto);
+
+    const imgProducto = document.createElement("img")
+    imgProducto.src = `../img/${producto.nombre}.png`;
+    imgProducto.width = 150
+    imgProducto.height = 150
+    imgProducto.style.borderRadius = "5px"
+    imgProducto.style.display = "block"
+    imgProducto.style.margin = "auto" 
+    imgProducto.style.marginBottom = "10px";
+    imgProducto.style.marginTop = "10px";
+    imgProducto.style.objectFit = "cover";
+
+    div_card.appendChild(imgProducto)
+    
+    const stockProducto = document.createElement("p");
+    stockProducto.innerHTML = `Stock: ${producto.stock}`;
+    div_card.appendChild(stockProducto);
+    
+    const precioProducto = document.createElement("p");
+    precioProducto.innerHTML = `Precio Unitario: $${producto.precioUnitario}`;
+    div_card.appendChild(precioProducto);
+    
+    divContainer_stock.appendChild(div_card);
+  });
     console.log(productos);
   }).catch(error => {
     console.error('ERROR AL CARGAR EL ARCHIVO JSON POR LO SIGUIENTE:', error);
